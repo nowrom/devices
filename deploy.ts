c791e6336149f8447ec2a2d50176859af24d2110
Tricked <72335827+SkyBlockDev@users.noreply.github.com>
2022-01-18 19:47:31 +0100

    update deploy.ts

--- deploy.ts
+++ deploy.ts
@@ -1,28 +1,92 @@
-const devices = Deno.readTextFile('./devices.json');
-const port = 4269;
-async function handleHttp(conn: Deno.Conn) {
-	for await (const e of Deno.serveHttp(conn)) {
-		if (new URL(e.request.url).pathname == '/roms') {
-			const roms = Deno.readTextFile('./roms.json');
-			const r = new Response(await roms, {
-				headers: {
-					'content-type': 'application/json',
-					'Access-Control-Allow-Origin': '*',
+import { serve } from 'https://deno.land/std@0.121.0/http/server.ts';
+import Fuse from 'https://deno.land/x/fuse@v6.4.1/dist/fuse.esm.min.js';
+import {
+	AugmentedRequest,
+	createRouteMap,
+	createRouter,
+} from 'https://deno.land/x/reno@v2.0.22/reno/mod.ts';
+
+const PORT = 8000;
+
+const devices = JSON.parse(await Deno.readTextFile('./devices.json'));
+
+function phoneParse(req: AugmentedRequest) {
+	let re = [...devices];
+	const q = req.queryParams.get('q');
+	const brand = req.queryParams.get('brand');
+	const limit = req.queryParams.get('limit');
+	const updateRe = (ar: any) => {
+		re.length = 0;
+		re.push(...ar);
+	};
+	if (q) {
+		const fuse = new Fuse(devices, {
+			keys: [
+				'brand',
+				'codename',
+				{
+					name: 'name',
+					weight: 2,
 				},
-			});
-			e.respondWith(r);
-		} else {
-			const r = new Response(await devices, {
-				headers: {
-					'content-type': 'application/json',
-					'Access-Control-Allow-Origin': '*',
-				},
-			});
-			e.respondWith(r);
-		}
+			],
+		});
+		updateRe(fuse.search(q).map((x) => x.item));
 	}
+	if (brand) {
+		updateRe(re.filter((x) => x.brand.toLowerCase() === brand.toLowerCase()));
+	}
+	if (limit && parseInt(limit)) {
+		updateRe(re.slice(0, parseInt(limit)));
+	}
+	const r = new Response(JSON.stringify(re), {
+		headers: {
+			'content-type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+		},
+	});
+	return r;
 }
-console.log(`Listening http://localhost:${port}`);
-for await (const conn of Deno.listen({ port })) {
-	handleHttp(conn);
-}
+
+export const routes = createRouteMap([
+	[
+		'/',
+		(req) => {
+			return phoneParse(req);
+		},
+	],
+	[
+		'/device',
+		(req) => {
+			return phoneParse(req);
+		},
+	],
+	[
+		'/roms',
+		async () => {
+			const r = new Response(await Deno.readFile('./roms.json'), {
+				headers: {
+					'content-type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+				},
+			});
+			return r;
+		},
+	],
+]);
+
+const router = createRouter(routes);
+
+console.log(`Listening for requests on port ${PORT}...`);
+
+await serve(
+	async (req) => {
+		try {
+			return await router(req);
+		} catch (e) {
+			return new Response('woa');
+		}
+	},
+	{
+		port: PORT,
+	}
+);
